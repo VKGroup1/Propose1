ea6af87da2d0feb82aad6292679b386a76331a34
VKGroup1 <155010512+VKGroup1@users.noreply.github.com>
2025-02-08 10:08:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".proposal-btn").addEventListener("click", function() {
+    document.querySelector(".hidden-message").style.display = "block";
+    this.style.display = "none"; // Hide button after clicking
+});
+
